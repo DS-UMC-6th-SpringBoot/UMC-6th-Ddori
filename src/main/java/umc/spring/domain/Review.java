b3154cabd003ec35d2c0c76259cfb8b9e300aa8c
heeyun817 <vmffpfm79@naver.com>
2024-05-15 15:55:33 +0900

    first commit

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,37 @@
+package umc.spring.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 50)
+  private String title;
+
+  @Column(nullable = false)
+  private Float score;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+}
